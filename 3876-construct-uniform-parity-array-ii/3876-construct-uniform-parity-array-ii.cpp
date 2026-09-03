96ed19ace33d41b0172bb0148c5ed5dd6ef6f351
Sujal Singh Negi <sujalnegi657@gmail.com>
2026-09-03 21:49:43 +0530

    Time: 5 ms (54.71%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+// ham odd number isliye search karenge kyunki hame even-odd krke odd bna skte hai to agar sbse chota odd mil jaye toh hame all odd vecctor form kar skte hai.
     bool uniformArray(vector<int>& nums1) {
         int m=INT_MAX;
         //find the minimu odd number in nums1
         for (int x : nums1) {
-            if (x % 2 ==1) {
+            if (x % 2 !=0) {
                 m = min(m,x);
             }
         }
